a46425e33016660714e4c0087b8a53423b9dd52d
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2023-11-20 01:18:31 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoDatos/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ProyectoDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textid = new System.Windows.Forms.TextBox();
+            this.textnombre = new System.Windows.Forms.TextBox();
+            this.textprecio = new System.Windows.Forms.TextBox();
+            this.textcolor = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btnmodificar = new System.Windows.Forms.Button();
+            this.dginstrumento = new System.Windows.Forms.DataGridView();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.textelim = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dginstrumento)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Color";
+            // 
+            // textid
+            // 
+            this.textid.Location = new System.Drawing.Point(198, 68);
+            this.textid.Name = "textid";
+            this.textid.Size = new System.Drawing.Size(188, 22);
+            this.textid.TabIndex = 4;
+            this.textid.TextChanged += new System.EventHandler(this.textid_TextChanged);
+            // 
+            // textnombre
+            // 
+            this.textnombre.Location = new System.Drawing.Point(198, 136);
+            this.textnombre.Name = "textnombre";
+            this.textnombre.Size = new System.Drawing.Size(188, 22);
+            this.textnombre.TabIndex = 5;
+            this.textnombre.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textprecio
+            // 
+            this.textprecio.Location = new System.Drawing.Point(198, 199);
+            this.textprecio.Name = "textprecio";
+            this.textprecio.Size = new System.Drawing.Size(188, 22);
+            this.textprecio.TabIndex = 6;
+            this.textprecio.TextChanged += new System.EventHandler(this.textprecio_TextChanged);
+            // 
+            // textcolor
+            // 
+            this.textcolor.Location = new System.Drawing.Point(198, 266);
+            this.textcolor.Name = "textcolor";
+            this.textcolor.Size = new System.Drawing.Size(188, 22);
+            this.textcolor.TabIndex = 7;
+            this.textcolor.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(530, 66);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 8;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(36, 321);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(75, 23);
+            this.btnguardar.TabIndex = 9;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // btnmodificar
+            // 
+            this.btnmodificar.Location = new System.Drawing.Point(198, 321);
+            this.btnmodificar.Name = "btnmodificar";
+            this.btnmodificar.Size = new System.Drawing.Size(75, 23);
+            this.btnmodificar.TabIndex = 10;
+            this.btnmodificar.Text = "Modificar";
+            this.btnmodificar.UseVisualStyleBackColor = true;
+            this.btnmodificar.Click += new System.EventHandler(this.btnmodificar_Click);
+            // 
+            // dginstrumento
+            // 
+            this.dginstrumento.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dginstrumento.Location = new System.Drawing.Point(12, 350);
+            this.dginstrumento.Name = "dginstrumento";
+            this.dginstrumento.RowHeadersWidth = 51;
+            this.dginstrumento.RowTemplate.Height = 24;
+            this.dginstrumento.Size = new System.Drawing.Size(593, 272);
+            this.dginstrumento.TabIndex = 12;
+            this.dginstrumento.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dginstrumento_CellContentClick);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(342, 321);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 13;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // textelim
+            // 
+            this.textelim.Location = new System.Drawing.Point(434, 321);
+            this.textelim.Name = "textelim";
+            this.textelim.Size = new System.Drawing.Size(188, 22);
+            this.textelim.TabIndex = 14;
+            this.textelim.TextChanged += new System.EventHandler(this.textelim_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 623);
+            this.Controls.Add(this.textelim);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.dginstrumento);
+            this.Controls.Add(this.btnmodificar);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.textcolor);
+            this.Controls.Add(this.textprecio);
+            this.Controls.Add(this.textnombre);
+            this.Controls.Add(this.textid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dginstrumento)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textid;
+        private System.Windows.Forms.TextBox textnombre;
+        private System.Windows.Forms.TextBox textprecio;
+        private System.Windows.Forms.TextBox textcolor;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btnmodificar;
+        private System.Windows.Forms.DataGridView dginstrumento;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.TextBox textelim;
+    }
+}
+
---
+++ ProyectoDatos/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProyectoDatos
+{
+    public partial class Form1 : Form
+    {
+        private InstrumentoMusical[] instrumentos = new InstrumentoMusical[10];
+        private int contadorInstrumentos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dginstrumento.Columns.Add("Id", "ID");
+            dginstrumento.Columns.Add("Nombre", "Nombre");
+            dginstrumento.Columns.Add("Precio", "Precio");
+            dginstrumento.Columns.Add("Color", "Color");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            // Evento Click del Label3
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            // Evento TextChanged del TextBox2
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            // Evento TextChanged del TextBox4
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Evento Click del Button4
+        }
+
+        private void dginstrumento_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Evento CellContentClick del DataGridView
+        }
+
+        private void textid_TextChanged(object sender, EventArgs e)
+        {
+            // Evento TextChanged del TextBox ID
+        }
+
+        private void textprecio_TextChanged(object sender, EventArgs e)
+        {
+            // Evento TextChanged del TextBox Precio
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            AgregarInstrumento();
+        }
+
+        private void btnmodificar_Click(object sender, EventArgs e)
+        {
+            ModificarInstrumento();
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            BuscarInstrumento();
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            EliminarInstrumento();
+        }
+
+        private void AgregarInstrumento()
+        {
+            if (contadorInstrumentos < instrumentos.Length)
+            {
+                int id = Convert.ToInt32(textid.Text);
+                string nombre = textnombre.Text;
+                double precio = Convert.ToDouble(textprecio.Text);
+                string color = textcolor.Text;
+
+                InstrumentoMusical nuevoInstrumento = new InstrumentoMusical
+                {
+                    Id = id,
+                    Nombre = nombre,
+                    Precio = precio,
+                    Color = color
+                };
+
+                instrumentos[contadorInstrumentos] = nuevoInstrumento;
+                contadorInstrumentos++;
+
+                MostrarInstrumentos();
+            }
+            else
+            {
+                MessageBox.Show("No se pueden agregar más instrumentos. Límite alcanzado.");
+            }
+        }
+
+        private void MostrarInstrumentos()
+        {
+            dginstrumento.Rows.Clear();
+
+            for (int i = 0; i < contadorInstrumentos; i++)
+            {
+                dginstrumento.Rows.Add(instrumentos[i].Id, instrumentos[i].Nombre, instrumentos[i].Precio, instrumentos[i].Color);
+            }
+        }
+
+        private void ModificarInstrumento()
+        {
+        }
+
+        private void BuscarInstrumento()
+        {
+        }
+
+        private void EliminarInstrumento()
+        {
+            int idEliminar = Convert.ToInt32(textelim.Text);
+
+            int indiceInstrumento = -1;
+
+            // Buscar el índice del instrumento a eliminar en el arreglo
+            for (int i = 0; i < contadorInstrumentos; i++)
+            {
+                if (instrumentos[i].Id == idEliminar)
+                {
+                    indiceInstrumento = i;
+                    break;
+                }
+            }
+
+            if (indiceInstrumento != -1)
+            {
+                // Mover los elementos siguientes para eliminar el instrumento
+                for (int i = indiceInstrumento; i < contadorInstrumentos - 1; i++)
+                {
+                    instrumentos[i] = instrumentos[i + 1];
+                }
+
+                contadorInstrumentos--;
+                MostrarInstrumentos();
+                MessageBox.Show("Instrumento eliminado correctamente.");
+            }
+            else
+            {
+                MessageBox.Show("Instrumento no encontrado.");
+            }
+        }
+
+        private void textelim_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
+    // Clase que representa un instrumento musical
+    public class InstrumentoMusical
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public string Color {  get; set; }
+    }
+
